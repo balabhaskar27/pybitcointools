fb6df372ae265226bc260cce7652a1f0e8ebe794
Chris Pacia <ctpacia@gmail.com>
2016-01-25 12:55:15 -0500

    update manifest

--- setup.py
+++ setup.py
@@ -5,13 +5,13 @@ except ImportError:
     from distutils.core import setup
 
 setup(name='bitcointools',
-      version='1.1.42',
+      version='1.1.44',
       description='Python Bitcoin Tools',
       author='Vitalik Buterin',
       author_email='vbuterin@gmail.com',
-      url='http://github.com/vbuterin/bitcointools',
+      url='http://github.com/vbuterin/pybitcointools',
       packages=['bitcointools'],
       scripts=['pybtctool'],
       include_package_data=True,
-      data_files=[("", ["LICENSE"]), ("bitcointools", ["bitcointools/english.txt"])],
+      data_files=[("bitcointools", ["bitcointools/english.txt"])],
       )
